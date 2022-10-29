496ee6623db90497f336e79782815a16041dea07
akash-268 <aakbajpai268@gmail.com>
2022-10-29 14:29:41 +0530

    minimize command

--- webDemo.py
+++ webDemo.py
@@ -3,8 +3,13 @@ from selenium.webdriver.chrome.service import Service
 
 service_obj = Service(r"C:\Users\Akash\Downloads\chromedriver_win32\chromedriver.exe")
 driver = webdriver.Chrome(service=service_obj)
+driver.maximize_window()
 
 driver.get("https://www.youtube.com/")
 print(driver.title)
 print(driver.current_url)
+
+driver.get("https://www.google.com/")
+driver.minimize_window()
+
 driver.close()
