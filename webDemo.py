cf2dc23483c63e0c63d8502ad4140997ac505bc0
akash-268 <aakbajpai268@gmail.com>
2022-10-29 14:35:28 +0530

    firefox and edge drivers

--- webDemo.py
+++ webDemo.py
@@ -1,8 +1,18 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 
+# -- Chrome
 service_obj = Service(r"C:\Users\Akash\Downloads\chromedriver_win32\chromedriver.exe")
 driver = webdriver.Chrome(service=service_obj)
+
+# -- Firefox
+# service_obj = Service(r"download and install gecko driver and give it's path")
+# driver = webdriver.Firefox(service=service_obj)
+
+# -- Microsoft Edge
+# service_obj = Service(r"download and install microsoft edge driver and give it's path")
+# driver = webdriver.Edge(service=service_obj)
+
 driver.maximize_window()
 
 driver.get("https://www.youtube.com/")
@@ -10,7 +20,7 @@ print(driver.title)
 print(driver.current_url)
 
 driver.get("https://www.google.com/")
-driver.minimize_window()
+#driver.minimize_window()
 
 driver.back()
 driver.refresh()
