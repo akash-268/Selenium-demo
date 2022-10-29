50986f7cc9fa6bfed0c48825cfe1cb1cf7da727c
akash-268 <aakbajpai268@gmail.com>
2022-10-29 15:03:02 +0530

    Xpath CSS

--- formDemo.py
+++ formDemo.py
@@ -8,7 +8,6 @@ driver = webdriver.Chrome(service=service_obj)
 
 driver.get("https://rahulshettyacademy.com/angularpractice/")
 
-driver.find_element(By.NAME, "name").send_keys("Akash Bajpai")
 driver.find_element(By.NAME, "email").send_keys("akashbajpai@gmail.com")
 driver.find_element(By.ID, "exampleInputPassword1").send_keys("aka475pai")
 driver.find_element(By.ID, "exampleCheck1").click()
@@ -20,3 +19,15 @@ if age < 18:
     driver.find_element(By.ID, "inlineRadio1").click()
 else:
     driver.find_element(By.ID, "inlineRadio2").click()
+
+# XPath => //tag-name[@attribute='value] -> //input[@type='submit']
+driver.find_element(By.XPATH, "//input[@type='submit']").click()
+
+# CSS Selector => tag-name[attribute='value'] -> input[type='submit]
+driver.find_element(By.CSS_SELECTOR, "input[name='name']").send_keys("Akash Bajpai")
+
+message = driver.find_element(By.CLASS_NAME, "alert-success").text
+
+assert "Success" in message
+
+driver.close()
