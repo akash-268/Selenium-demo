d1f8ffaebb85d505607557891910b64d30e85e76
akash-268 <aakbajpai268@gmail.com>
2022-10-31 23:40:49 +0530

    minor 2

--- waitDemo.py
+++ waitDemo.py
@@ -5,6 +5,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
 
+actual = []
+expected = ['Cucumber - 1 Kg', 'Raspberry - 1/4 Kg', 'Strawberry - 1/4 Kg']
 service_obj = Service(r"C:\Users\Akash\Downloads\chromedriver_win32\chromedriver.exe")
 driver = webdriver.Chrome(service=service_obj)
 driver.implicitly_wait(5)
@@ -15,9 +17,16 @@ driver.find_element(By.CSS_SELECTOR, "input[type='search']").send_keys(query)
 time.sleep(1)
 li = driver.find_elements(By.CSS_SELECTOR,
                           "div[class='products'] div[class='product'] div[class='product-action'] button")
+
+li2 = driver.find_elements(By.CSS_SELECTOR,
+                           "div[class='products'] div[class='product'] div[c"
+                           "lass='product-action'] h4")
+
+for i in li2:
+    actual.append(i.text)
 for i in li:
     i.click()
-
+print(actual)
 driver.find_element(By.CLASS_NAME, "cart-icon").click()
 driver.find_element(By.CSS_SELECTOR, "div[class='action-block'] button").click()
 
@@ -29,11 +38,8 @@ for i in prices:
 print(sum)
 
 x = int(driver.find_element(By.CLASS_NAME, "totAmt").text)
-if x == sum:
-    print("YES")
-else:
-    print("NO")
 assert x == sum
+
 # driver.find_elements(By.CLASS_NAME,"amount").
 driver.find_element(By.CSS_SELECTOR, "div[class='promoWrapper'] input[class='promoCode']").send_keys(
     "rahulshettyacademy")
@@ -41,3 +47,6 @@ driver.find_element(By.CLASS_NAME, "promoBtn").click()
 wait = WebDriverWait(driver, 20)
 wait.until(expected_conditions.presence_of_element_located((By.CSS_SELECTOR, ".promoInfo")))
 assert driver.find_element(By.CLASS_NAME, "promoInfo").is_displayed()
+
+y = float(driver.find_element(By.CLASS_NAME, "discountAmt").text)
+assert y < x
